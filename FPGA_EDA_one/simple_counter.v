d6fcdfdb723914eb4e86a36a6c596434720e8e6a
chengqi <chengqi0908@foxmail.com>
2020-04-08 10:59:51 +0800

    1

--- FPGA_EDA_one/simple_counter.v
+++ FPGA_EDA_one/simple_counter.v
@@ -1,12 +1,12 @@
-module simple_counter(
+module simple_counter(                                //模块定义
 	CLOCK_50,
 	counter_out
 );
-input CLOCK_50;
-output[31:0]counter_out;
-reg [31:0]counter_out;
-always @ (posedge CLOCK_50)
+input CLOCK_50;                                       //输入时钟
+output[31:0]counter_out;                              //输出引脚
+reg [31:0]counter_out;                 
+always @ (posedge CLOCK_50)                   			//上升沿有效
 begin
-counter_out<=#1 counter_out+1;
+counter_out<=#1 counter_out+1;              				//计数器功能实现
 end
 endmodule
